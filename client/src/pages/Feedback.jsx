4c5ad7e21f05bf72bf9e0f589fdd5eb89e3dc192
Madhav <spmadhav04@gmail.com>
2025-07-20 03:44:06 +0530

    ok

--- client/src/pages/Feedback.jsx
+++ client/src/pages/Feedback.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-<<<<<<< HEAD
 const Feedback = () => {
   return (
     <div>Feedback</div>
@@ -8,12 +7,3 @@ const Feedback = () => {
 }
 
 export default Feedback
-=======
-export default function Feedback() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
->>>>>>> 44b8c659b979ab9767a57af9b40b6726ba46c43f
